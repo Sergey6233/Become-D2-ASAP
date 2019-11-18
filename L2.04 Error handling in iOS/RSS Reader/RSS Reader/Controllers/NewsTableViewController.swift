6f3e48b8e52fa112cd3b03e4244c168a55b2c295
Sergey Bizunov <sergey_bizunov@epam.com>
2019-11-18 13:01:03 +0300

    L2.04 Error handling in iOS, RSS Reader  - initial commit

---
+++ L2.04 Error handling in iOS/RSS Reader/RSS Reader/Controllers/NewsTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewsTableViewController.swift
+//  RSS Reader
+//
+//  Created by Sergey Bizunov on 11/18/19.
+//  Copyright © 2019 Sergey Bizunov. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+
+    private struct Constants {
+        static let grodnoRssUrlPath = "https://news.tut.by/rss/geonews/grodno.rss"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
